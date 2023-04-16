da9bfd73278f215b9fd4befea5239d0e10f8c732
Julia Faqir <julia.faqir@pwc.com>
2023-04-16 13:30:44 +0300

    Initial commit

---
+++ app/adminDashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './styles.module.css'
+
+export default function adminDashboard() {
+  return (
+    <div className={styles.main}>
+    <h1>
+      Admin page
+    </h1>
+    </div>
+  )
+}
